526586cb8d08a7e0163075763db7823eb6757fdd
Mirera <phudongsi1@gmail.com>
2023-07-07 14:44:54 +0700

    12th

--- app/src/main/java/com/bt/navigation/GiftFrag.java
+++ app/src/main/java/com/bt/navigation/GiftFrag.java
@@ -1,5 +1,6 @@
 package com.bt.navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +16,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class GiftFrag extends Fragment {
+
+    TextView tvName;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +63,11 @@ public class GiftFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gift, container, false);
+        View view =  inflater.inflate(R.layout.fragment_gift, container, false);
+        tvName = (TextView) view.findViewById(R.id.tvGift);
+        Intent intent = new Intent();
+        String Name = intent.getStringExtra("Name");
+        tvName.setText("Hello " + Name);
+        return view;
     }
 }
