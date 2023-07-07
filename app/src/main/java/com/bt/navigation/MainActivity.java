b15db37c09d08307102f84fc6a4d27c3a0750d75
Mirera <phudongsi1@gmail.com>
2023-07-07 14:05:24 +0700

    6th

--- app/src/main/java/com/bt/navigation/MainActivity.java
+++ app/src/main/java/com/bt/navigation/MainActivity.java
@@ -1,14 +1,42 @@
 package com.bt.navigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.frameFrag);
+        bottomNav = (BottomNavigationView) findViewById(R.id.BottomNav);
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.shop){
+                    
+                } else if (id == R.id.gift) {
+                    
+                } else if (id == R.id.cart) {
+                    
+                } else if (id == R.id.profile) {
+                    
+                }
+                return false;
+            }
+        });
     }
 }
